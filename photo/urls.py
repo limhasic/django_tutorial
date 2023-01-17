f6ec483b2f6dd6a027f0cdf71427c586e157d547
DESKTOP-TN7S3K0\gems <oilbread@naver.com>
2023-01-17 13:44:12 +0900

    Django 모델, 템플릿, 뷰 생성

--- photo/urls.py
+++ photo/urls.py
@@ -3,9 +3,14 @@ from . import views
 # 현재 폴더에서 views를 가져오라는 의미
 
 urlpatterns = [
-    path('', views.photo_list, name = 'photo_list')
-    path('', views.photo_list, name='photo_list')
+    path('', views.photo_list, name = 'photo_list'),
+    path('photo/<int:pk>/', views.photo_detail, name = 'photo_detail'),
+
 ]
+
+# pk -> 정수형 변수가 들어감
+# 기본 ID값으로 구분
+#
 
 # path = > 127.0.0.1:8000
 # 현재 폴더에서 views.py를 가져오고 photo_list 함수 실행 name은 photo_list
--- photo/views.py
+++ photo/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from .models import Photo
+from django.shortcuts import render, get_object_or_404
+# 모델로부터 데이터를 찾아보고 없으면 404 에러를 반환하는 함수
 
 def photo_list(request):
     photos = Photo.objects.all()
@@ -8,11 +10,13 @@ def photo_list(request):
 # {P} 데이터 전달 시 사용 #
 # 함수 photo_list() 호출 시 photo_list.html이 불러와짐
 
-from django.shortcuts import render, get_object_or_404
-# 모델로부터 데이터를 찾아보고 없으면 404 에러를 반환하는 함수
 
-def photo_detail(request, pk:
-    photo = get_object_or_404(Photo, pk)
+def photo_detail(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
     return render(request, 'photo/photo_detail.html', {'photo':photo})
+
+
+# pk : 모델의 데이터들을 구분하는 django 의 기본 ID 값
+# -> 데이터를 photo_detail.html로 보냄
 
 # 추가하고는 view에 등록
